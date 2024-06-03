dcda3f84a6bb2668dd30c5aaa7059fbf41198874
Nicoco220983 <nicolas.carrez@gmail.com>
2024-06-03 10:29:44 +0200

    git_config_perso.sh

---
+++ git_config_perso.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -x
+git config --add --local user.email nicolas.carrez@gmail.com
+git config --add --local user.name Nicoco220983
+git config --add --local core.sshCommand 'ssh -i ~/.ssh/id_rsa_ncarrez_github'
